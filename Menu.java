ccea8012e89456c2306c348c71aa016393230e2a
Samuel Diaz <samuelsami1717@gmail.com>
2019-11-12 08:06:35 -0600

    add

--- Menu.java
+++ Menu.java
@@ -4,20 +4,20 @@ import javax.swing.*;
 import javax.swing.filechooser.*;
 import java.io.*;   
 public class Menu{
-
+Label lb;
 JButton start;
 JFrame  frame;
 
 public Menu(){
- 
+ frame = new JFrame ("Pokegochi");
+ start = new JButton ("Start");
+
+frame.add(start);
 }
 
 public void menu(){
-frame = new JFrame ("Pokegochi");
-start = new JButton ("Start");
 
-frame.add
-start.add
+
 
 frame.setVisible(true);
 frame.setSize(600,600);

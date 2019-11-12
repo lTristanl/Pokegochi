63ef0998ec803651d7e114e519d542ec44d3c0b5
Samuel Diaz <samuelsami1717@gmail.com>
2019-11-12 10:20:55 -0600

    Actualizacion de Menu

--- Menu.java
+++ Menu.java
@@ -3,27 +3,38 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.filechooser.*;
 import java.io.*;   
+
 public class Menu{
+JLabel lab;
 JButton start;
 JFrame  frame;
+JPanel panel;
 
 public Menu(){
  frame = new JFrame ("Pokegochi");
  start = new JButton ("Start");
+ panel = new JPanel();
+ lab = new JLabel("POKEGOCHI");
 
-frame.add(start);
+frame.add(panel, BorderLayout.SOUTH);
+frame.add(lab,BorderLayout.CENTER);
+panel.add(start);
+
+frame.setVisible(true);
+frame.setSize(600,600);
+
 }
-
 public void menu(){
 
 
 
-frame.setVisible(true);
-frame.setSize(600,600);
+
 }
 
+public static void main(String args[]){
+Menu obj=new Menu();
 
 
 
-
+}
 }
